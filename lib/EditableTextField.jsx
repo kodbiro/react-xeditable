a22e064cf9fcccaccecb57cfbde540456b3c1e14
Aram Drevekenin <aram@onetwotrade.com>
2015-12-09 00:52:03 +0200

    Some fixes for latest react version

--- lib/EditableTextField.jsx
+++ lib/EditableTextField.jsx
@@ -11,7 +11,9 @@ var ButtonToolbar = require('react-bootstrap').ButtonToolbar;
 
 var EditableTextField = React.createClass({
     save: function() {
-        this.props.onUpdate(this.props.name, this.refs.input.getValue());
+        if (this.props.onUpdate) {
+          this.props.onUpdate(this.props.name, this.refs.input.getValue());
+        };
         this.refs.overlay.hide();
     },
 
@@ -26,17 +28,17 @@ var EditableTextField = React.createClass({
 
     render: function() {
     	var empty = this.props.value === "";
+      var id = this.props.id !== "undefined" ? this.props.id : "popover";
     	var linkText = empty ? 'Empty' : this.props.value;
     	var linkClass = empty ? 'editable-click editable-empty' : 'editable-click';
         var popover = 
-            <Popover>
+            <Popover id={id}>
                 <form className='form-inline' onSubmit={this.submit}>
-                <Input type='text' ref='input' placeholder='Empty' className='input-sm' defaultValue={this.props.value}>
+                <Input type='text' ref='input' placeholder='Empty' className='input-sm' defaultValue={this.props.value} />
                 <ButtonToolbar className='editable-buttons'>
                 <Button bsStyle='primary' className='btn-sm' onClick={this.save}><i className='glyphicon glyphicon-ok'></i></Button>
                 <Button bsStyle='default' className='btn-sm' onClick={this.cancel}><i className='glyphicon glyphicon-remove'></i></Button>
                 </ButtonToolbar>
-                </Input>
                 </form>
             </Popover>;
 
